008c50b342c44bddeb0d99cf7dd7a23b0cf97c25
Ifat <iribon9@gmail.com>
2018-06-15 09:36:53 -0500

    refactor

--- src/js/main/apiActions.js
+++ src/js/main/apiActions.js
@@ -1,15 +1,17 @@
 import { requestWithKey } from 'lp-redux-api'
 
+const { API_ENDPOINT } = process.env
+
 export const REQ_SCHEDULE = 'REQ_SCHEDULE'
 
 export function fetchSchedule () {
   return requestWithKey(REQ_SCHEDULE, {
-    url: `/${process.env.API_ENDPOINT}/Schedule?view=viwt1WOG8hR71EX9q`
+    url: `/${API_ENDPOINT}/Schedule?view=viwt1WOG8hR71EX9q`
   })
 }
 
 export function fetchSarahSchedule () {
   return requestWithKey(REQ_SCHEDULE, {
-    url: `/${process.env.API_ENDPOINT}/SarahSchedule?view=viw0ypJ98ne5lqm80`
+    url: `/${API_ENDPOINT}/SarahSchedule?view=viw0ypJ98ne5lqm80`
   })
 }
